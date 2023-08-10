2659dfc418caa38c0761c7e1b1bd9d4146600d9f
Luiz Souza <luiz.h.32112@hotmail.com>
2023-08-10 15:41:14 -0300

    2.7

---
+++ Site Colégio/wwwroot/js/JSL.js
@@ -0,0 +1,20 @@
+﻿// scripts.js
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[index].classList.add('active');
+    currentSlide = index;
+}
+
+function nextSlide() {
+    const nextIndex = (currentSlide + 1) % slides.length;
+    showSlide(nextIndex);
+}
+
+function startAutomaticSlideshow() {
+    setInterval(nextSlide, 3000); // Troca de slide a cada 3 segundos
+}
+
+window.onload = startAutomaticSlideshow;
